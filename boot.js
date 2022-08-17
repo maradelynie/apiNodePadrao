f1c7ea5a18f67bdff9f00d4da2a196bc2ba57c5f
Mara Oliveira <mara.oliveira@leomadeiras.com.br>
2022-08-16 22:20:47 -0300

    DB conection

--- boot.js
+++ boot.js
@@ -1,5 +1,15 @@
 module.exports	=	app	=>	{
-  app.listen(app.get('port'),	()	=>	{
-      console.log(`NTask	API	-	porta	${app.get('port')}`);
-  });
+  async	function	start(port) {
+      try	{
+          await	app.db.authenticate();
+          await	app.db.sync();
+          app.listen(port,	()	=>	{
+              console.log(`NTask	API	-	porta	${port}`);
+          });
+      }	catch	(err)	{
+          console.log('Erro	de	conexão	com	banco	de	dados.');
+          console.error(err);
+      }
+  }
+  start(app.get('port'));
 }
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const	express	=	require('express');
 const	consign	=	require('consign');
 const	app	=	express();
+
 consign()
-		.include('models')
+		.include('db.js')
+		.then('models')
 		.then('middlewares.js')
 		.then('routes')
 		.then('boot.js')
