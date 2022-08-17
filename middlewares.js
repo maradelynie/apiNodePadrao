9290ca2e3308ca561d3b8311c6c361a7d9092f54
Mara Oliveira <mara.oliveira@leomadeiras.com.br>
2022-08-17 00:05:54 -0300

    refactore and creation of users and tasks endpoints

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,11 @@
+const	bodyParser	=	require('body-parser');
+
 module.exports	=	app	=>	{
   app.set('port',	3000);
-  app.set('json spaces',	2);
+  app.set('json	spaces',	2);
+  app.use(bodyParser.json());
+  app.use((req,	res,	next)	=>	{
+      delete	req.body.id;
+      next();
+  });
 };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,56 @@
-module.exports	=	app	=>	{
-  const	Tasks	=	app.models.tasks;
-  app.get('/tasks',	async	(req,	res)	=>	{
-      try	{
-          const	tasks	=	await	Tasks.findAll();
-          res.json({	tasks	});
-      }	catch	(err)	{
-        res.status(500).json(err);
+module.exports = app => {
+  const Tasks = app.models.tasks;
+  app.route('/tasks')
+    .get(async (req, res) => {
+      try {
+        const result = await Tasks.findAll();
+        res.json(result);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
       }
-  });
+    })
+    .post(async (req, res) => {
+      try {
+        const result = await Tasks.create(req.body);
+        res.json(result);
+      } catch (err) {
+        res.status(412).json({ msg: err.message });
+      }
+    });
+    app.route('/tasks/:id')
+		.get(async	(req,	res)	=>	{
+				try	{
+						const	{	id	}	=	req.params;
+						const	where	=	{	id	};
+						const	result	=	await	Tasks.findOne({	where	});
+						if	(result)	{
+								res.json(result);
+						}	else	{
+								res.sendStatus(404);
+						}
+				}	catch	(err)	{
+						res.status(412).json({	msg:	err.message	});
+				}
+		})
+    .put(async	(req,	res)	=>	{
+      try	{
+          const	{	id	}	=	req.params;
+          const	where	=	{	id	};
+          await	Tasks.update(req.body,	{	where	});
+          res.sendStatus(204);
+      }	catch	(err)	{
+          res.status(412).json({	msg:	err.message	});
+      }
+    })
+    .delete(async	(req,	res)	=>	{
+      try	{
+          const	{	id	}	=	req.params;
+          const	where	=	{	id	};
+          await	Tasks.destroy({	where	});
+          res.sendStatus(204);
+      }	catch	(err)	{
+          res.status(412).json({	msg:	err.message	});
+      }
+});
+
 };
